58f3606fc5b7d25bfe6533764aac76946dd8118c
shweta kc <devshwetakc@hotmail.com>
2022-01-20 13:27:44 +0545

    add intro page

---
+++ udemy/lib/screen/onBording.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import 'package:udemy/color.dart';
+import 'package:udemy/model/onBording.dart';
+
+class OnBordoing extends StatefulWidget {
+
+  @override
+  _OnBordoingState createState() => _OnBordoingState();
+}
+
+class _OnBordoingState extends State<OnBordoing> {
+  final _introKey = GlobalKey<IntroductionScreenState>();
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      onDone: (){},
+      key: _introKey,
+      showSkipButton:true,
+      skipFlex:0,
+      nextFlex: 0,
+      skip: Text('Skip', style: TextStyle(color: primary),),
+      next: Icon(Icons.arrow_forward, color: primary,),
+      done: Text('Done', style: TextStyle(color: primary),),
+      pages: List.generate(onBoardings.length, (index){
+        return PageViewModel(
+          title: onBoardings[index]['title'],
+          body: onBoardings[index]['description'],
+          image: Image.asset(onBoardings[index]['imageUrl']),
+        );
+      }),
+      
+    );
+  }
+}
