ed26553d99d45fb3aa5ba03828607a1566822d06
shweta kc <devshwetakc@hotmail.com>
2022-01-20 14:02:03 +0545

    Add menu button

--- udemy/lib/screen/root.dart
+++ udemy/lib/screen/root.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:udemy/color.dart';
 
 class Root extends StatefulWidget {
-
   @override
   _RootState createState() => _RootState();
 }
@@ -16,9 +16,26 @@ class _RootState extends State<Root> {
     );
   }
 
-  AppBar getAppBar(){
+  AppBar getAppBar() {
     return AppBar(
-      
+      backgroundColor: white,
+      elevation: 0,
+      flexibleSpace: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              splashRadius: 25,
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  'assets/images/menu.svg',
+                  height: 24,
+                ))
+          ],
+        ),
+      )),
     );
   }
 }
