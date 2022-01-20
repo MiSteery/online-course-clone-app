9d77c89b5ba3ff93d97d8a22980ce68d77aade7e
shweta kc <devshwetakc@hotmail.com>
2022-01-20 13:32:27 +0545

    Add decoration , background color

--- udemy/lib/screen/onBording.dart
+++ udemy/lib/screen/onBording.dart
@@ -5,7 +5,6 @@ import 'package:udemy/color.dart';
 import 'package:udemy/model/onBording.dart';
 
 class OnBordoing extends StatefulWidget {
-
   @override
   _OnBordoingState createState() => _OnBordoingState();
 }
@@ -15,22 +14,40 @@ class _OnBordoingState extends State<OnBordoing> {
   @override
   Widget build(BuildContext context) {
     return IntroductionScreen(
-      onDone: (){},
+      onDone: () {},
       key: _introKey,
-      showSkipButton:true,
-      skipFlex:0,
+      globalBackgroundColor: white,
+      showSkipButton: true,
+      skipFlex: 0,
       nextFlex: 0,
-      skip: Text('Skip', style: TextStyle(color: primary),),
-      next: Icon(Icons.arrow_forward, color: primary,),
-      done: Text('Done', style: TextStyle(color: primary),),
-      pages: List.generate(onBoardings.length, (index){
+      skip: Text(
+        'Skip',
+        style: TextStyle(color: primary),
+      ),
+      next: Icon(
+        Icons.arrow_forward,
+        color: primary,
+      ),
+      done: Text(
+        'Done',
+        style: TextStyle(color: primary),
+      ),
+      pages: List.generate(onBoardings.length, (index) {
         return PageViewModel(
           title: onBoardings[index]['title'],
           body: onBoardings[index]['description'],
           image: Image.asset(onBoardings[index]['imageUrl']),
+          decoration: PageDecoration(
+            titleTextStyle:
+                TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            bodyTextStyle: TextStyle(
+              fontSize: 16,
+              color: grey,
+            ),
+            descriptionPadding: EdgeInsets.all(40),
+          ),
         );
       }),
-      
     );
   }
 }
