88c6ea3dcd1f24bf32ee88dce0744623860c7502
shweta kc <devshwetakc@hotmail.com>
2022-01-21 15:04:15 +0545

    Add hello username

--- udemy/lib/screen/home.dart
+++ udemy/lib/screen/home.dart
@@ -1,13 +1,51 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:udemy/color.dart';
+import 'package:udemy/model/constant.dart';
 
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('home'),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return ListView(
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                  color: grey,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                username,
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
