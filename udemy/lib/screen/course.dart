70a7aef2e0b6f349b78140795a377192cfcf7905
shweta kc <devshwetakc@hotmail.com>
2022-01-21 13:32:35 +0545

    Add course page

---
+++ udemy/lib/screen/course.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Course extends StatelessWidget {
+  const Course({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('course'),
+    );
+  }
+}
