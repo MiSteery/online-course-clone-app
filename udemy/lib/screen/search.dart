35738c4bc785fa7308626b83dbfdc236f985278a
shweta kc <devshwetakc@hotmail.com>
2022-01-26 11:33:49 +0545

    Add icon and search text

--- udemy/lib/screen/search.dart
+++ udemy/lib/screen/search.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:udemy/color.dart';
 
 class Search extends StatelessWidget {
-  const Search({ Key? key }) : super(key: key);
+  const Search({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('search'),
+        backgroundColor: white,
+        appBar: getAppBar(),
+        body: Center(
+          child: Text('Search Page'),
+        ));
+  }
+
+  AppBar getAppBar() {
+    return AppBar(
+      backgroundColor: white,
+      elevation: 0,
+      title: Text(
+        'Search',
+        style: TextStyle(
+          color: black,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.shopping_cart_outlined,
+            color: black,
+          ),
+        )
+      ],
     );
   }
 }
