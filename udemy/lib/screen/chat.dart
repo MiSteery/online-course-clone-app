c4767b044508c262ed190f1aef4cd0ce2f921214
shweta kc <devshwetakc@hotmail.com>
2022-01-27 12:18:11 +0545

    Add search box

--- udemy/lib/screen/chat.dart
+++ udemy/lib/screen/chat.dart
@@ -3,18 +3,19 @@ import 'package:flutter/material.dart';
 import '../color.dart';
 
 class Chat extends StatelessWidget {
-  const Chat({ Key? key }) : super(key: key);
+  const Chat({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: white,
       appBar: getAppBar(),
-      body: Text('chat'),
+      body: getBody(size),
     );
   }
 
-   AppBar getAppBar() {
+  AppBar getAppBar() {
     return AppBar(
       backgroundColor: white,
       elevation: 0,
@@ -33,13 +34,33 @@ class Chat extends StatelessWidget {
             color: black,
           ),
         ),
-          IconButton(
+        IconButton(
           onPressed: () {},
           icon: Icon(
             Icons.more_vert,
             color: black,
           ),
         )
+      ],
+    );
+  }
+
+  Widget getBody(Size) {
+    return ListView(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Container(
+              height: 45,
+              width: Size.width - 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(width: 1, color: grey),
+              ),
+            )),
       ],
     );
   }
