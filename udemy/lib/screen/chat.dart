b52b1390ccefc5d30fd8629f37c095f3964553fa
shweta kc <devshwetakc@hotmail.com>
2022-01-27 12:10:49 +0545

    Add chat text , notification icon , more icon

--- udemy/lib/screen/chat.dart
+++ udemy/lib/screen/chat.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../color.dart';
+
 class Chat extends StatelessWidget {
   const Chat({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
+      appBar: getAppBar(),
       body: Text('chat'),
+    );
+  }
+
+   AppBar getAppBar() {
+    return AppBar(
+      backgroundColor: white,
+      elevation: 0,
+      centerTitle: true,
+      title: Text(
+        'chat',
+        style: TextStyle(
+          color: black,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.notifications,
+            color: black,
+          ),
+        ),
+          IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.more_vert,
+            color: black,
+          ),
+        )
+      ],
     );
   }
 }
