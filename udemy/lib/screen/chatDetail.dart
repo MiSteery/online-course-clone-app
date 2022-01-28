3351a350f6fa629c7d11a934bc18ff76b60c65a2
shweta kc <devshwetakc@hotmail.com>
2022-01-28 10:30:04 +0545

    Add app bar

--- udemy/lib/screen/chatDetail.dart
+++ udemy/lib/screen/chatDetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy/color.dart';
 
 class ChatDetail extends StatelessWidget {
   const ChatDetail({ Key? key }) : super(key: key);
@@ -6,7 +7,13 @@ class ChatDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
+      appBar: getAppBar(),
       body: Text('chat detail'),
     );
+  }
+
+  AppBar getAppBar(){
+    return AppBar();
   }
 }
