1e1e959eaf830b407faaa9beced348364f763911
shweta kc <devshwetakc@hotmail.com>
2022-01-28 13:16:36 +0545

    add profile text

--- udemy/lib/screen/account.dart
+++ udemy/lib/screen/account.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:udemy/color.dart';
 
 class Account extends StatelessWidget {
-  const Account({ Key? key }) : super(key: key);
+  const Account({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: white,
+      appBar: getAppBar(),
       body: Text('account'),
     );
+  }
+
+  AppBar getAppBar(){
+    return AppBar(
+      centerTitle: true,
+        backgroundColor: white,
+        elevation: 0,
+        title:Text('Profile', style: TextStyle(color: black),
+       )
+      );
   }
 }
