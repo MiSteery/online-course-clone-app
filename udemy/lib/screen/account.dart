382e42bfda669c47fa3074d112d4532e9f3c144a
shweta kc <devshwetakc@hotmail.com>
2022-01-28 13:55:48 +0545

    add viceo icon

--- udemy/lib/screen/account.dart
+++ udemy/lib/screen/account.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:udemy/color.dart';
+import 'package:udemy/model/account.dart';
 import 'package:udemy/model/constant.dart';
 
 class Account extends StatelessWidget {
@@ -7,10 +8,11 @@ class Account extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: white,
       appBar: getAppBar(),
-      body: getBody(),
+      body: getBody(size),
     );
   }
 
@@ -32,7 +34,7 @@ class Account extends StatelessWidget {
     );
   }
 
-  Widget getBody() {
+  Widget getBody(size) {
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -42,34 +44,36 @@ class Account extends StatelessWidget {
           Center(
             child: Column(
               children: [
-              Stack(
-                clipBehavior: Clip.none,
-                 children: [
-                Container(
-                  height: 95,
-                  width: 95,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      image: DecorationImage(
-                          image: NetworkImage(profileUrl), fit: BoxFit.cover,
-                          ),
-                          ),
-                ),
-                Positioned(
-                  bottom: -4,
-                  right: -4,
-                  child: Container(
-                    height: 26,
-                    width: 26,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: primary,
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Container(
+                      height: 95,
+                      width: 95,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: NetworkImage(profileUrl),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
-                    child: Icon(Icons.edit_outlined, color: white, size: 16),
-                  ),
+                    Positioned(
+                      bottom: -4,
+                      right: -4,
+                      child: Container(
+                        height: 26,
+                        width: 26,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: primary,
+                        ),
+                        child:
+                            Icon(Icons.edit_outlined, color: white, size: 16),
+                      ),
+                    ),
+                  ],
                 ),
-                 ],
-              ),
                 SizedBox(
                   height: 10,
                 ),
@@ -91,8 +95,50 @@ class Account extends StatelessWidget {
                   ),
                 )
               ],
-              ),                
+            ),
           ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Wrap(
+              spacing: 15,
+              children: [
+                Container(
+                  height: 60,
+                  width: (size.width - 61) / 3,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: primary,
+                      boxShadow: [
+                        BoxShadow(
+                          color: primary.withOpacity(0.5),
+                          blurRadius: 5,
+                          offset: Offset(0, 5),
+                        )
+                      ]),
+                  child: Column(
+                    children: [
+                      Icon(
+                        userAchieves[0]['icon'],
+                        color: white,
+                        size: 25,
+                      ),
+                      Text(
+                        userAchieves[0]['title'],
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: white,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
